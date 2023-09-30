34b4dd386bbbbd229d9add019064a99c244a2acd
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2023-09-30 11:23:19 +0200

    Add files via upload

---
+++ cdn/js/index.js
@@ -0,0 +1,33 @@
+const cuttedPage = window.location.href.split("/");
+document.addEventListener("DOMContentLoaded", async function() {
+    document.getElementById("openBtn").onclick = function() {
+        document.getElementById("sideNav").classList.add("active");
+    };
+    document.getElementById("closeBtn").onclick = function() {
+        document.getElementById("sideNav").classList.remove("active");
+    };
+    
+    const resplang = { "en": "English", "fr": "Français", "es": "Español" };
+    for (const lang in resplang) {
+        if (Object.hasOwnProperty.call(resplang, lang)) {
+            const element = document.createElement("option");
+            element.id = lang;
+            element.value = lang;
+            element.innerText = resplang[lang];
+            if (lang === cuttedPage[window.location.href.split("/").length-2]) document.getElementById("lang").ariaSelected = `${lang}`;
+            document.getElementById("lang").appendChild(element);
+        }
+    }
+    
+    for (let i = 0; i < document.getElementById("lang").options.length; i++) {
+        if (document.getElementById("lang").options[i].value === cuttedPage[window.location.href.split("/").length-2]) {
+            document.getElementById("lang").selectedIndex = i;
+            break;
+        }
+    }
+    
+    document.getElementById("lang").addEventListener("change", function() {
+        if (cuttedPage[window.location.href.split("/").length-2] === event.target.id) return;
+        window.location.href = `https://${cuttedPage[window.location.href.split("/").length-3]}/${event.target.value ?? event.target.id}/index.html`;
+    });
+});
