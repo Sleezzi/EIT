9f1c65fd8a039939c3785d903537ed763a98da49
Sleezzi <ilan.dfp91@gmail.com>
2023-11-29 13:45:08 +0100

    Update

--- cdn/js/LampLed.js
+++ cdn/js/LampLed.js
@@ -1,4 +1,4 @@
-const cuttedPage = window.location.href.split("/");
+const product = window.location.href.split("/")[window.location.href.split("/").length-1].replace(".html", "");
 if (localStorage.getItem("theme") === undefined || localStorage.getItem("theme") === null) localStorage.setItem("theme", "dark");
 if (localStorage.getItem("theme") === "dark") {
     document.getElementById("theme").querySelector("img").src = `./cdn/img/sun.png`;
@@ -30,14 +30,14 @@ document.getElementById("image").onmouseup = function() {
     const image = localStorage.getItem("image");
     if (JSON.parse(image).current === JSON.parse(image).max) {
         localStorage.setItem("image", JSON.stringify({ current: 1, max: JSON.parse(image).max }));
-        this.innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${cuttedPage[window.location.href.split("/").length-1].replace(".html", "")}/1.png">`;
+        this.innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${product}/1.png">`;
     } else {
         localStorage.setItem("image", JSON.stringify({ current: JSON.parse(image).current+1, max: JSON.parse(image).max }));
-        this.innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${cuttedPage[window.location.href.split("/").length-1].replace(".html", "")}/${JSON.parse(image).current+1}.png">`;
+        this.innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${product}/${JSON.parse(image).current+1}.png">`;
     }
 }
 fetch(`./cdn/products.json`, { method: "GET" }).then(resp => {if (resp.status === 200) return resp.json()}).then(response => {
-    const data = response[cuttedPage[`${window.location.href.split("/").length-1}`].replace(".html", "")][cuttedPage[window.location.href.split("/").length-2 ?? document.querySelector("html").lang]];
+    const data = response[product][localStorage.getItem("lang")];
     
     const img = document.createElement("img");         
     img.style = "height: 25%; width: 10%; margin: 2px;";
@@ -46,17 +46,17 @@ fetch(`./cdn/products.json`, { method: "GET" }).then(resp => {if (resp.status ==
     span.style = "color: black;";
     span.ariaLabel = data.buy;
     span.innerHTML = data.buy;
-    document.getElementById("title").innerText = data.name ?? cuttedPage[window.location.href.split("/").length-1].replace(".html", "");
+    document.getElementById("title").innerText = data.name ?? product;
     document.getElementById("description").innerText = data.description;
     document.getElementById("price").innerText = data.price;
     document.getElementById("buy").appendChild(img);
     document.getElementById("buy").appendChild(span);
     document.getElementById("home").setAttribute("href", `./${cuttedPage[window.location.href.split("/").length-2]}/index.html`);
-    document.title = `${cuttedPage[`${window.location.href.split("/").length-3}`]} - ${cuttedPage[window.location.href.split("/").length-1].replace(".html", "")}`;
+    document.title = `${cuttedPage[`${window.location.href.split("/").length-3}`]} - ${product}`;
     document.getElementById("charTitle").innerText = data.char[0].title;
     const style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = `./${cuttedPage[window.location.href.split("/").length-1].replace(".html", "")}.css`;
+    style.href = `./${cuttedPage}.css`;
     document.head.appendChild(style);
     data.char.forEach(char => {
         if (!char.name || !char.value) return;
@@ -65,6 +65,6 @@ fetch(`./cdn/products.json`, { method: "GET" }).then(resp => {if (resp.status ==
         span.innerHTML = `${char.name}: ${char.value}`;
         document.getElementById("charContent").appendChild(span);
     });
-    localStorage.setItem("image", JSON.stringify({ current: 1, max: response[cuttedPage[window.location.href.split("/").length-1].replace(".html", "")].pictures.length }));
-    document.getElementById("image").innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${cuttedPage[window.location.href.split("/").length-1].replace(".html", "")}/1.png">`;
+    localStorage.setItem("image", JSON.stringify({ current: 1, max: response[product].pictures.length }));
+    document.getElementById("image").innerHTML = `<img style="height: 100%;width: 100%;" src="./cdn/products/${product}/1.png">`;
 });
